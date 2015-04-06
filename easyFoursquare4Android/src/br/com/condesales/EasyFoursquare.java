c4b8e4c12b25ccb5ecbed05074c781c573ea60c7
Doğa Öz <doga.ozkaraca@gmail.com>
2015-04-06 23:26:35 +0300

    Update EasyFoursquare.java
    yeah,you're right.I was working on that a month ago,most probably i was testing logout variations to make sure.  So, it's better to use like that.

--- easyFoursquare4Android/src/br/com/condesales/EasyFoursquare.java
+++ easyFoursquare4Android/src/br/com/condesales/EasyFoursquare.java
@@ -57,25 +57,13 @@ public class EasyFoursquare {
     /**
      * Logout and remove access token from shared prefs, so another user can log in later
      */
-    public int logout()
+    public void logout()
     {
-    	
-    	
-    	try
-    	{
-        SharedPreferences settings = mActivity.getSharedPreferences(
-                FoursquareConstants.SHARED_PREF_FILE, 0);
+    	SharedPreferences settings = mActivity.getSharedPreferences(FoursquareConstants.SHARED_PREF_FILE, 0);
         Editor editor = settings.edit();
         editor.putString(FoursquareConstants.ACCESS_TOKEN, "");
         editor.commit();
-        
-    	}
-    	catch(Exception ee)
-    	{
-    		return 1;
-    	}
-		return 0;	
-    }
+     }
     /**
      * Requests logged user information asynchronously.
      *
